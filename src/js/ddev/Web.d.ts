945d6bb601c4c4897fa09c55f50ba3f7453f4c81
MatsM16 <matdyr23@gmail.com>
2019-02-10 18:26:59 +0100

    Rewrote web request handling

--- src/js/ddev/Web.d.ts
+++ src/js/ddev/Web.d.ts
@@ -1,37 +1,23 @@
 export declare namespace Web {
-    namespace _Helpers {
-        function isSuccess(status: number): boolean;
-        function serialize(data: any, type: DataType): any;
-        function deserialize(data: any, type: DataType): any;
-        function applyHeaders(request: XMLHttpRequest, headers?: Map<string, string>, type?: DataType): void;
-    }
+    function send(method: RequestMethod, url: string, responseType?: RequestDataType, data?: any, dataType?: RequestDataType, headerSettings?: RequestHeaderSettings): Promise<any>;
+    function setGlobalHeader(name: string, value: string): void;
+    function get(url: string, responseType?: RequestDataType): Promise<any>;
 }
 export declare namespace Web {
-    function send(method: RequestType, url: string, responseType?: DataType, data?: any, dataType?: DataType, headers?: Map<string, string>): Promise<any>;
-    function get(url: string, responseType?: DataType, headers?: Map<string, string>): Promise<any>;
-    function post(url: string, data?: any, dataType?: DataType, headers?: Map<string, string>): Promise<any>;
-    function put(url: string, data?: any, dataType?: DataType, headers?: Map<string, string>): Promise<any>;
-    function del(url: string, headers?: Map<string, string>): Promise<any>;
+    type RequestMethod = "GET" | "PUT" | "POST" | "DELETE";
+    type RequestDataType = "TEXT" | "JSON" | "XML" | "BLOB" | "BYTEARRAY";
+    type RequestHeaderSettings = {
+        useBlobalHeaders?: boolean;
+        customHeaders?: Map<string, string>;
+    };
+}
+export declare namespace Web.Requests {
     const globalHeaders: Map<string, string>;
-    type DataType = "JSON" | "TEXT" | "BINARY" | "FILE";
-    type RequestType = "GET" | "POST" | "PUT" | "DELETE";
-}
-export declare namespace Web {
-    class Api {
-        private _headers;
-        private _functions;
-        constructor(functions?: Map<string, ApiFunction>, headers?: Map<string, string>);
-        setHeader(name: string, value: string): void;
-        setFunction(f: ApiFunction): void;
-        call(name: string, data?: any, ...args: string[]): Promise<any>;
-        _consoleView(): void;
-    }
-    interface ApiFunction {
-        name: string;
-        url: string;
-        method: Web.RequestType;
-        dataType?: Web.DataType;
-        responseType?: Web.DataType;
-        urlParameterNames?: string[];
-    }
+    function createRequest(method: RequestMethod, url: string): XMLHttpRequest;
+    function applyHeaderSettings(xhr: XMLHttpRequest, settings: RequestHeaderSettings): void;
+    function applyResponseType(xhr: XMLHttpRequest, type: RequestDataType): void;
+    function handleError(reject: Function, { status, statusText }: XMLHttpRequest): void;
+    function isSuccessfull({ status }: XMLHttpRequest): boolean;
+    function deserializeResponse({ response }: XMLHttpRequest, responseType: RequestDataType): any;
+    function serializeData(data: any, type: RequestDataType): null | undefined;
 }
--- src/js/ddev/Web.js
+++ src/js/ddev/Web.js
@@ -1,177 +1,113 @@
-// Helper functions
+//
+// Basic functionality
+//
 export var Web;
 (function (Web) {
-    let _Helpers;
-    (function (_Helpers) {
-        function isSuccess(status) {
-            return (status >= 200 && status < 300) || status === 304;
-        }
-        _Helpers.isSuccess = isSuccess;
-        function serialize(data, type) {
-            type = type.toUpperCase();
-            if (type === "TEXT")
-                return "" + data;
-            if (type === "JSON")
-                return JSON.stringify(data);
-            if (type === "BINARY")
-                return data;
-            if (type === "FILE")
-                return data;
-            throw new Error("Unsupported type");
-        }
-        _Helpers.serialize = serialize;
-        function deserialize(data, type) {
-            type = type.toUpperCase();
-            if (type === "TEXT")
-                return "" + data;
-            if (type === "JSON")
-                return JSON.parse(data);
-            if (type === "BINARY")
-                return new Blob(data);
-            if (type === "FILE")
-                return new File(data, "file");
-            throw new Error("Unsupported type");
-        }
-        _Helpers.deserialize = deserialize;
-        function applyHeaders(request, headers, type) {
-            //
-            // Global headers
-            //
-            for (const [name, value] of Web.globalHeaders)
-                request.setRequestHeader(name, value);
-            //
-            // Specified headers
-            //
-            if (headers)
-                for (const [name, value] of headers)
-                    request.setRequestHeader(name, value);
-            //
-            // Type spesific headers
-            //
-            if (!type)
-                return;
-            type = type.toUpperCase();
-            if (type === "JSON")
-                request.setRequestHeader("content-type", "application/json");
-            if (type === "TEXT")
-                request.setRequestHeader("content-type", "text/plain");
-            if (type === "BINARY" || type === "FILE")
-                request.setRequestHeader("content-type", "application/octet-stream");
-        }
-        _Helpers.applyHeaders = applyHeaders;
-    })(_Helpers = Web._Helpers || (Web._Helpers = {}));
-})(Web || (Web = {}));
-// Request handling
-(function (Web) {
-    function send(method, url, responseType, data, dataType, headers) {
-        method = method.toUpperCase();
-        return new Promise((resolve, reject) => {
-            const request = new XMLHttpRequest();
-            request.open(method, url);
-            Web._Helpers.applyHeaders(request, headers, dataType);
-            request.onload = () => {
-                if (!Web._Helpers.isSuccess(request.status)) {
-                    reject({ message: `Request failed: [${request.status}] - ${request.statusText}` });
-                    return;
+    function send(method, url, responseType, data, dataType, headerSettings) {
+        return new Promise((reject, resolve) => {
+            const request = Web.Requests.createRequest(method, url);
+            if (responseType)
+                Web.Requests.applyResponseType(request, responseType);
+            if (dataType)
+                data = Web.Requests.serializeData(data, dataType);
+            if (headerSettings)
+                Web.Requests.applyHeaderSettings(request, headerSettings);
+            request.onload = function () {
+                if (!Web.Requests.isSuccessfull(request))
+                    Web.Requests.handleError(reject, request);
+                else {
+                    const response = !responseType
+                        ? request.response
+                        : Web.Requests.deserializeResponse(request, responseType);
+                    resolve(response);
                 }
-                let response = request.response;
-                if (responseType !== undefined)
-                    response = Web._Helpers.deserialize(response, responseType);
-                resolve(response);
             };
-            request.onerror = () => {
-                reject({ message: `Request failed: [${request.status}] - ${request.statusText}` });
-            };
-            if (dataType !== undefined && data !== undefined && data !== null)
-                data = Web._Helpers.serialize(data, dataType);
-            try {
-                request.send(data);
-            }
-            catch (_a) {
-                reject({ message: `Request failed: [${request.status}] - ${request.statusText}` });
-            }
+            request.onerror = () => Web.Requests.handleError(reject, request);
+            request.send(data);
         });
     }
     Web.send = send;
-    function get(url, responseType, headers) {
-        return send("GET", url, responseType, undefined, undefined, headers);
+    function setGlobalHeader(name, value) {
+        Web.Requests.globalHeaders.set(name, value);
+    }
+    Web.setGlobalHeader = setGlobalHeader;
+    function get(url, responseType) {
+        return send("GET", url, responseType);
     }
     Web.get = get;
-    function post(url, data, dataType, headers) {
-        return send("POST", url, undefined, data, dataType, headers);
-    }
-    Web.post = post;
-    function put(url, data, dataType, headers) {
-        return send("PUT", url, undefined, data, dataType, headers);
-    }
-    Web.put = put;
-    function del(url, headers) {
-        return send("DELETE", url, undefined, undefined, undefined, headers);
-    }
-    Web.del = del;
-    Web.globalHeaders = new Map();
 })(Web || (Web = {}));
-// Api
+//
+// Request handling
+//
 (function (Web) {
-    class Api {
-        constructor(functions, headers) {
-            //
-            // Functions
-            //
-            if (functions !== undefined)
-                this._functions = functions;
+    var Requests;
+    (function (Requests) {
+        Requests.globalHeaders = new Map();
+        function createRequest(method, url) {
+            const xhr = new XMLHttpRequest();
+            xhr.open(method, url);
+            return xhr;
+        }
+        Requests.createRequest = createRequest;
+        function applyHeaderSettings(xhr, settings) {
+            if (settings.useBlobalHeaders === true)
+                for (const [name, value] of Requests.globalHeaders)
+                    xhr.setRequestHeader(name, value);
+        }
+        Requests.applyHeaderSettings = applyHeaderSettings;
+        function applyResponseType(xhr, type) {
+            switch (type) {
+                case "BLOB":
+                    xhr.responseType = "blob";
+                    break;
+                case "BYTEARRAY":
+                    xhr.responseType = "arraybuffer";
+                    break;
+                case "JSON":
+                    xhr.responseType = "json";
+                    break;
+                case "TEXT":
+                    xhr.responseType = "text";
+                    break;
+                case "XML":
+                    xhr.responseType = "document";
+                    break;
+                default:
+                    throw new Error("Unsupported type " + type);
+            }
+        }
+        Requests.applyResponseType = applyResponseType;
+        function handleError(reject, { status, statusText }) {
+            const reason = {
+                message: statusText,
+                code: status
+            };
+            reject(reason);
+        }
+        Requests.handleError = handleError;
+        function isSuccessfull({ status }) {
+            if (status >= 200 && status < 300)
+                return true;
+            else if (status === 304)
+                return true;
             else
-                this._functions = new Map();
-            //
-            // Headers 
-            //
-            if (headers !== undefined)
-                this._headers = headers;
+                return false;
+        }
+        Requests.isSuccessfull = isSuccessfull;
+        function deserializeResponse({ response }, responseType) {
+            if (responseType === "BYTEARRAY")
+                return new Uint8Array(response);
+            else if (responseType === "TEXT")
+                return new String(response);
             else
-                this._headers = new Map();
+                return response;
         }
-        setHeader(name, value) {
-            this._headers.set(name, value);
+        Requests.deserializeResponse = deserializeResponse;
+        function serializeData(data, type) {
+            if (data === null || data === undefined)
+                return null;
         }
-        setFunction(f) {
-            this._functions.set(f.name, f);
-        }
-        call(name, data, ...args) {
-            const func = this._functions.get(name);
-            if (!func)
-                throw new Error(`Function not defined: ${name}`);
-            let url = func.url;
-            if (func.urlParameterNames && args.length > 0) {
-                for (let i = 0; i < args.length && i < func.urlParameterNames.length; i++) {
-                    let name = func.urlParameterNames[i];
-                    let value = args[i];
-                    if (i === 0)
-                        url += `?${name}=${value}`;
-                    else
-                        url += `&${name}=${value}`;
-                }
-            }
-            return Web.send(func.method, url, func.responseType, data, func.dataType, this._headers);
-        }
-        _consoleView() {
-            const fArr = [];
-            for (let entry of this._functions) {
-                if (entry[1].urlParameterNames)
-                    //@ts-ignore
-                    entry[1].urlParameterNames = entry[1].urlParameterNames.join(", ");
-                fArr.push(entry[1]);
-            }
-            const hArr = [];
-            for (let entry of this._headers) {
-                hArr.push({ name: entry[0], value: entry[1] });
-            }
-            console.log("--- FUNCTIONS ---");
-            console.table(fArr);
-            console.log("");
-            console.log("--- HEADERS ---");
-            console.table(hArr);
-        }
-    }
-    Web.Api = Api;
+        Requests.serializeData = serializeData;
+    })(Requests = Web.Requests || (Web.Requests = {}));
 })(Web || (Web = {}));
 //# sourceMappingURL=Web.js.map
