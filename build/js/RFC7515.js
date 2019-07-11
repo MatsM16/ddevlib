55e44630723f2f42859d988cd48a796988c48dbf
MatsM16 <matdyr23@gmail.com>
2019-07-12 00:39:01 +0200

    No clue what this is anyways

--- build/js/RFC7515.js
+++
@@ -1,6 +0,0 @@
-import { Binary } from "./Binary";
-export function toRFC7515(guid) {
-    return Binary.read(Binary.writeSync(guid.data, "text/plain"), "base64");
-}
-export function fromRFC7515(rfc7515) {
-}
